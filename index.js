44bebe83c4543141757e886640338cf7c045fc99
tausifabid12 <tausifabid12@gmail.com>
2023-07-17 10:20:30 +0600

    initial commit

--- index.js
+++ index.js
@@ -22,7 +22,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var _a;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const xlsx = __importStar(require("xlsx"));
@@ -37,13 +45,14 @@ function excelToJson(inputFile, outputFile) {
     // Convert the sheet to JSON format
     // Convert the sheet to JSON format
     const jsonData = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
-    console.log(jsonData);
     // Convert the JSON data to a string
     const jsonString = JSON.stringify(jsonData, null, 2);
     // Write the JSON data to a file
     fs.writeFileSync(outputFile, jsonString, 'utf-8');
 }
 // excelToJson('input.xlsx', 'output.json');
+const newData = [];
+const unMatchedData = [];
 // Function to read JSON file
 function readJsonFile(filePath) {
     // Read the JSON file
@@ -65,23 +74,141 @@ function fuzzySearch(string1, string2) {
     }
     return false;
 }
-// try {
-//   const jsonData = readJsonFile('collegedata.json');
-//   console.log('JSON data:', jsonData[0].name);
-// } catch (error) {
-//   console.error('Error reading JSON file:', error);
-// }
 const keys = readJsonFile('outputKey.json');
-console.log('JSON data:', keys);
-const collegedata = readJsonFile('collegedata.json');
-const CollegeName = (_a = collegedata[0]) === null || _a === void 0 ? void 0 : _a.name;
-try {
-    const jsonData = readJsonFile('output.json');
-    console.log('JSON data:', jsonData[2][4]);
+// const collegedata = readJsonFile('collegedata.json');
+let collegedata;
+//exal
+const exalData = readJsonFile('output.json');
+const exalDataLength = exalData.length;
+function getcollege() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch("http://127.0.0.1:4000/", {
+            method: "POST",
+            body: JSON.stringify({
+                query: `
+      query Campuses {
+        campuses {
+          id
+          name
+        }
+      }
+      `
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const { data } = yield response.json();
+        collegedata = data;
+        console.log(data, 'this i sdata');
+        return data;
+    });
 }
-catch (error) {
-    console.error('Error reading JSON file:', error);
+const UpdateCollege = () => __awaiter(void 0, void 0, void 0, function* () {
+    const data = yield getcollege();
+    const collegeData = data === null || data === void 0 ? void 0 : data.campuses;
+    console.log(collegeData, 'collegeData');
+});
+UpdateCollege();
+// for (let i = 0; i < exalData.length; i++) {
+//   const string1 = exalData[i][4];
+//   for (let j = 0; j < collegedata.length; j++) {
+//     const string2 = collegedata[j].name;
+//     const isMatch = fuzzySearch(string1, string2);
+//     if (isMatch) {
+//       const strr = string1.split(' (Id:')
+//       const id = strr[1].slice(1, -1)
+//       updateCollege(id, string1, collegedata[j]?.name)
+//       const updattedData = { ...collegedata[j], name: string1, id: id }
+//       newData.push(updattedData)
+//     }
+//     else {
+//       unMatchedData.push(collegedata[j]?.name)
+//     }
+//   }
+// }
+// console.log(newData, newData.length, 'newData')
+// console.log(unMatchedData, unMatchedData.length, 'unMatchedData')
+// const mergedArray = [...newData, ...collegedata];
+// const uniqueArray = mergedArray.filter(
+//   (item, index, self) =>
+//     index ===
+//     self.findIndex(
+//       (t) => t.link === item.link && t.name === item.name
+//     )
+// );
+// console.log(uniqueArray.length, 'uniqueArray');
+// //geting exams
+function updateCollege(id, name, where) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch("http://localhost:4000/", {
+            method: "POST",
+            body: JSON.stringify({
+                query: `mutation UpdateCampuses($update: CampusUpdateInput) {
+        updateCampuses(update: $update) {
+          campuses {
+            is_main
+            name
+          }
+        }
+      }`,
+                variables: {
+                    update: {
+                        id: id,
+                        name: name
+                    },
+                    where: {
+                        name: where
+                    }
+                }
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const { data } = yield response.json();
+        console.log(data, 'exam');
+        return data;
+    });
 }
+// async function update3(id: string, name: string, where: string) {
+//   const response = await fetch("http://localhost:4000/", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       query: `mutation UpdateCampuses($where: CampusWhere, $update: CampusUpdateInput) {
+//         updateCampuses(where: $where, update: $update) {
+//           campuses {
+//             name
+//             id
+//           }
+//         }
+//       }`,
+//       variables: {
+//         where: {
+//           collegeHas: {
+//             name: null
+//           }
+//         },
+//         update: {
+//           name: null
+//         }
+//       }
+//     }),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+//   const { data } = await response.json();
+//   console.log(data, 'exam')
+//   return data;
+// }
+// const UpdateCollege = async () => {
+//   const data = await getcollege()
+//   const collegeData = data?.campuses
+//   console.log(collegeData, 'collegeData')
+// }
+// UpdateCollege()
+// getExams()
 // Usage example
 // const string1 = 'Aalim Muhammed Salegh College of Engineering,Chennai';
 // const string2 = 'Aalim Muhammed Salegh 3243  Engineering ';
--- index.tsx
+++ index.tsx
@@ -13,12 +13,8 @@ function excelToJson(inputFile: string, outputFile: string) {
 
   console.log('converting')
   // Convert the sheet to JSON format
-
-
   // Convert the sheet to JSON format
   const jsonData = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
-  console.log(jsonData);
-
   // Convert the JSON data to a string
   const jsonString = JSON.stringify(jsonData, null, 2);
 
@@ -28,6 +24,9 @@ function excelToJson(inputFile: string, outputFile: string) {
 
 // excelToJson('input.xlsx', 'output.json');
 
+
+const newData = []
+const unMatchedData = []
 
 
 // Function to read JSON file
@@ -58,43 +57,189 @@ function fuzzySearch(string1: string, string2: string): boolean {
   return false;
 }
 
-
-
-
-// try {
-//   const jsonData = readJsonFile('collegedata.json');
-//   console.log('JSON data:', jsonData[0].name);
-
-// } catch (error) {
-//   console.error('Error reading JSON file:', error);
-// }
-
 const keys = readJsonFile('outputKey.json');
-const collegedata = readJsonFile('collegedata.json');
-const CollegeName = collegedata[0]?.name
+// const collegedata = readJsonFile('collegedata.json');
+let collegedata: any
+
+
+
+
 
 //exal
 const exalData = readJsonFile('output.json');
-const collegeLength = collegedata.length
 const exalDataLength = exalData.length
 
-for (let i = 0; i < exalDataLength; i++) {
-  // Usage example
-  const string1 = exalData[i][0];
-  const string2 = CollegeName;
+async function getcollege() {
+  const response = await fetch("http://localhost:4000/", {
+    method: "POST",
+    body: JSON.stringify({
+      query: `
+      query Campuses {
+        campuses {
+          id
+          name
+        }
+      }
+      `
 
-  const isMatch = fuzzySearch(string1, string2);
-  console.log(`Match found: ${isMatch}`);
-  console.log(`Match found: ${exalData[i][0]}`, i);
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const { data } = await response.json();
+  collegedata = data
+  console.log(data, 'this i sdata')
+  return data;
+}
+
+const UpdateCollege = async () => {
+  const data = await getcollege()
+  const collegeData = data?.campuses
+  console.log(collegeData, 'collegeData', data)
+}
+
+UpdateCollege()
+
+
+
+
+// for (let i = 0; i < exalData.length; i++) {
+//   const string1 = exalData[i][4];
+
+//   for (let j = 0; j < collegedata.length; j++) {
+//     const string2 = collegedata[j].name;
+
+//     const isMatch = fuzzySearch(string1, string2);
+
+//     if (isMatch) {
+
+//       const strr = string1.split(' (Id:')
+//       const id = strr[1].slice(1, -1)
+//       updateCollege(id, string1, collegedata[j]?.name)
+
+//       const updattedData = { ...collegedata[j], name: string1, id: id }
+//       newData.push(updattedData)
+//     }
+//     else {
+//       unMatchedData.push(collegedata[j]?.name)
+//     }
+
+//   }
+// }
+
+// console.log(newData, newData.length, 'newData')
+
+
+// console.log(unMatchedData, unMatchedData.length, 'unMatchedData')
+
+// const mergedArray = [...newData, ...collegedata];
+
+// const uniqueArray = mergedArray.filter(
+//   (item, index, self) =>
+//     index ===
+//     self.findIndex(
+//       (t) => t.link === item.link && t.name === item.name
+//     )
+// );
+
+// console.log(uniqueArray.length, 'uniqueArray');
+
+
+
+
+
+
+
+
+
+
+
+// //geting exams
+async function updateCollege(id: string, name: string, where: string) {
+  const response = await fetch("http://localhost:4000/", {
+    method: "POST",
+    body: JSON.stringify({
+      query: `mutation UpdateCampuses($update: CampusUpdateInput) {
+        updateCampuses(update: $update) {
+          campuses {
+            is_main
+            name
+          }
+        }
+      }`,
+      variables: {
+        update: {
+          id: id,
+          name: name
+        },
+        where: {
+          name: where
+        }
+      }
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const { data } = await response.json();
+  console.log(data, 'exam')
+  return data;
 
 }
 
 
+// async function update3(id: string, name: string, where: string) {
+//   const response = await fetch("http://localhost:4000/", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       query: `mutation UpdateCampuses($where: CampusWhere, $update: CampusUpdateInput) {
+//         updateCampuses(where: $where, update: $update) {
+//           campuses {
+//             name
+//             id
+//           }
+//         }
+//       }`,
+//       variables: {
+//         where: {
+//           collegeHas: {
+//             name: null
+//           }
+//         },
+//         update: {
+//           name: null
+//         }
+//       }
+//     }),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+
+//   const { data } = await response.json();
+//   console.log(data, 'exam')
+//   return data;
+
+// }
 
 
 
 
 
+
+// const UpdateCollege = async () => {
+//   const data = await getcollege()
+//   const collegeData = data?.campuses
+//   console.log(collegeData, 'collegeData')
+// }
+
+// UpdateCollege()
+
+
+// getExams()
 
 
 // Usage example
